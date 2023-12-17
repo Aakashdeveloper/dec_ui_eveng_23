23e1d72ddd904914141d1357a6657eaef9644197
aakashdeveloper <ahanda205@gmail.com>
2023-12-17 22:55:29 +0530

    boot5

---
+++ javascript/datatype.js
@@ -0,0 +1,104 @@
+//es5
+//es6
+> Ecma Script
+
+DataType
+String ="Hii" 'hey' "true" 'false' "10" '11.5' `Hi`
+Number = 10 10.11 .22 23534464 33.32
+Boolean = true false
+
+//es5
+var a = 10
+var b = "Hii"
+var c = true
+
+typeof(a)
+'number'
+typeof(b)
+'string'
+typeof(c)
+'boolean'
+
+var d = "10"
+typeof(d)
+'string'
+var e = 10.33
+typeof(e)
+'number'
+
+var a = 10
+var b = 20
+a+b (addition)
+30
+a-b
+-10
+a*b
+200
+a/b
+0.5
+4%2
+0
+4%3
+1
+4%5
+4
+2%3
+2
+3%2
+1
+
+var a = "Hiii"
+var b = "JavaScript"
+
+a+b (concat)
+'HiiiJavaScript'
+a-b
+NaN (Not A Number)
+a*b
+NaN
+a/b
+NaN
+
+var c = 1
+undefined
+a+b
+'HiiiJavaScript'
+a+c
+'Hiii1'
+
+
+string +string = string
+string +number = string
+number +string = string
+number +number = number
+
+
+10+"20"+30
+"1020"+30
+"102030"
+
+10+20+"30"
+30 + "30"
+"3030"
+
+"10"+20+30
+"1020"+30
+"102030"
+
+10+"20"+30-1
+10+20+"30"-1
+"10"+20+30-1
+
+var a = "hii"
+var b = 1
+
+a-b
+NaN
+"hii"-1
+NaN
+10+"20"+30-1
+102029
+10+20+"30"-1
+3029
+"10"+20+30-1
+102029
